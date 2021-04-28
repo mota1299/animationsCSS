b24d6f20106ab6ee74c9d9d154e29f0cbe364651
Juan Jesús Ávila Mota <juanmotaavila@gmail.com>
2021-04-28 14:55:56 -0500

    Subir archivos

---
+++ funcion.js
@@ -0,0 +1,265 @@
+var t1 = just.animate({
+    targets: "#boton",
+    duration: 1300,
+    props: {
+      innerHTML: ['animación', '1'] 
+    },
+    web: {
+       transform: [
+          { offset: 0, value: "scale(1, 1)" },
+          { offset: 0.3, value: "scale(1.25, .75)" },
+          { offset: 0.4, value: "scale(.75, 1.25)" },
+          { offset: 0.5, value: "scale(1.15, .85)" },
+          { offset: 0.65, value: "scale(.95, 1.05)" },
+          { offset: 0.75, value: "scale(1.05, .95)" },
+          { offset: 1, value: "scale(1, 1)" }
+       ]
+    }
+ });
+ 
+ t1.play();
+ 
+ just.tools.player(t1);
+ /////
+ var t2 = just.animate({
+    targets: "#boton2",
+    duration: 1300,
+    props: {
+      innerHTML: ['animación', '2'] 
+    },
+    web: {
+       transform: [
+          { offset: 1, value: "scale(1, 1)" },
+          { offset: 0.1, value: "scale(.20, .20)" },
+          { offset: 0.2, value: "scale(.30, .30)" },
+          { offset: 0.3, value: "scale(.40, .40)" },
+          { offset: 0.4, value: "scale(.50, .50)" },
+          { offset: 0.5, value: "scale(.60, .60)" },
+          { offset: 0.6, value: "scale(.70, .70)" },
+          { offset: 0.7, value: "scale(.80, .80)" },
+          { offset: 0.8, value: "scale(.90, .90)" },
+          { offset: 1, value: "scale(1, 1)" }
+       ]
+    }
+ });
+ 
+ t2.play();
+ 
+ just.tools.player(t2);
+ 
+ ////////////////////////////////////
+ var t3 = just.animate({
+    targets: "#boton3",
+    duration: 1300,
+    props: {
+      innerHTML: ['animación', '3'] 
+    },
+    web: {
+       transform: [
+          { offset: 1, value: "scale(1, 1)" },
+          { offset: 0.9, value: "scale(.10, .10)" },
+          { offset: 0.8, value: "scale(.20, .20)" },
+          { offset: 0.7, value: "scale(.30, .30)" },
+          { offset: 0.6, value: "scale(.40, .40)" },
+          { offset: 0.5, value: "scale(.50, .50)" },
+          { offset: 0.4, value: "scale(.60, .60)" },
+          { offset: 0.3, value: "scale(.70, .70)" },
+          { offset: 0.2, value: "scale(.80, .80)" },
+          { offset: 0.1, value: "scale(.90, .90)" },
+          { offset: 0, value: "scale(1, 1)" }
+       ]
+    }
+ });
+ 
+ t3.play();
+ 
+ just.tools.player(t3);
+  ////////////////////////////////////
+  var t4 = just.animate({
+   targets: "#boton4",
+   duration: 2300,
+   props: {
+     innerHTML: ['animación', '4'] 
+   },
+   web: {
+      transform: [
+         { offset: 1, value: "scale(.10, .10)" },
+         { offset: 0.9, value: "scale(1, 1)" },
+         { offset: 0.8, value: "scale(.20, .20)" },
+         { offset: 0.7, value: "scale(.90, .90)" },
+         { offset: 0.6, value: "scale(.30, .30)" },
+         { offset: 0.5, value: "scale(.80, .80)" },
+         { offset: 0.4, value: "scale(.40, .40)" },
+         { offset: 0.3, value: "scale(.70, .70)" },
+         { offset: 0.2, value: "scale(.50, .50)" },
+         { offset: 0.1, value: "scale(.60, .60)" },
+         { offset: 0, value: "scale(1, 1)" }
+      ]
+   }
+});
+
+t4.play();
+
+just.tools.player(t4);
+  ////////////////////////////////////
+  var t5 = just.animate({
+   targets: "#boton5",
+   duration: 2300,
+   props: {
+     innerHTML: ['animación', '5'] 
+   },
+   web: {
+      transform: [
+         { offset: 1, value: "scale(1, 1)" },
+         { offset: 0.9, value: "scale(1, 1)" },
+         { offset: 0.8, value: "scale(.20, .20)" },
+         { offset: 0.7, value: "scale(.90, .90)" },
+         { offset: 0.6, value: "scale(.30, .30)" },
+         { offset: 0.5, value: "scale(.80, .80)" },
+         { offset: 0.4, value: "scale(.40, .40)" },
+         { offset: 0.3, value: "scale(.70, .70)" },
+         { offset: 0.2, value: "scale(.50, .50)" },
+         { offset: 0.1, value: "scale(.60, .60)" },
+         { offset: 0, value: "scale(.10, .10 )" }
+      ]
+   }
+});
+
+t5.play();
+
+just.tools.player(t5);
+  ////////////////////////////////////
+  var t6 = just.animate({
+   targets: "#boton6",
+   duration: 1000,
+   props: {
+     innerHTML: ['animación', '6'] 
+   },
+   web: {
+      transform: [
+         { offset: 1, value: "scale(1, 1)" },
+         { offset: 0.9, value: "scale(.10, 1)" },
+         { offset: 0.8, value: "scale(.20, 1)" },
+         { offset: 0.7, value: "scale(.30, 1)" },
+         { offset: 0.6, value: "scale(.40, 1)" },
+         { offset: 0.5, value: "scale(.50, 1)" },
+         { offset: 0.4, value: "scale(.60, 1)" },
+         { offset: 0.3, value: "scale(.70, 1)" },
+         { offset: 0.2, value: "scale(.80, 1)" },
+         { offset: 0.1, value: "scale(.90, 1)" },
+         { offset: 0, value: "scale(1, 1 )" }
+      ]
+   }
+});
+
+t6.play();
+
+just.tools.player(t6);
+ ////////////////////////////////////
+ var t7 = just.animate({
+   targets: "#boton7",
+   duration: 1000,
+   props: {
+     innerHTML: ['animación', '7'] 
+   },
+   web: {
+      transform: [
+         { offset: 1, value: "scale(1, 1)" },
+         { offset: 0.9, value: "scale(1, .10)" },
+         { offset: 0.8, value: "scale(1, .20)" },
+         { offset: 0.7, value: "scale(1, .30)" },
+         { offset: 0.6, value: "scale(1, .40)" },
+         { offset: 0.5, value: "scale(1, .50)" },
+         { offset: 0.4, value: "scale(1, .60)" },
+         { offset: 0.3, value: "scale(1, .70)" },
+         { offset: 0.2, value: "scale(1, .80)" },
+         { offset: 0.1, value: "scale(1, .90)" },
+         { offset: 0, value: "scale(1, 1 )" }
+      ]
+   }
+});
+
+t7.play();
+
+just.tools.player(t7);
+ ////////////////////////////////////
+ var t8 = just.animate({
+   targets: "#boton8",
+   duration: 1000,
+   props: {
+     innerHTML: ['animación', '8'] 
+   },
+   web: {
+      transform: [
+         { offset: 1, value: "scale(1, 1)" },
+         { offset: 0.9, value: "scale(1, .10)" },
+         { offset: 0.8, value: "scale(2, .20)" },
+         { offset: 0.7, value: "scale(1, .30)" },
+         { offset: 0.6, value: "scale(2, .40)" },
+         { offset: 0.5, value: "scale(1, .50)" },
+         { offset: 0.4, value: "scale(2, .60)" },
+         { offset: 0.3, value: "scale(1, .70)" },
+         { offset: 0.2, value: "scale(2, .80)" },
+         { offset: 0.1, value: "scale(1, .90)" },
+         { offset: 0, value: "scale(2, 1 )" }
+      ]
+   }
+});
+
+t8.play();
+
+just.tools.player(t8);
+ ////////////////////////////////////
+ var t9 = just.animate({
+   targets: "#boton9",
+   duration: 1000,
+   props: {
+     innerHTML: ['animación', '9'] 
+   },
+   web: {
+      transform: [
+         { offset: 1, value: "scale(1, 1)" },
+         { offset: 0.9, value: "scale(1, 2)" },
+         { offset: 0.8, value: "scale(2, 1)" },
+         { offset: 0.7, value: "scale(1, 2)" },
+         { offset: 0.6, value: "scale(2, 1)" },
+         { offset: 0.5, value: "scale(1, 2)" },
+         { offset: 0.4, value: "scale(2, 1)" },
+         { offset: 0.3, value: "scale(1, 2)" },
+         { offset: 0.2, value: "scale(2, 1)" },
+         { offset: 0.1, value: "scale(1, 2)" },
+         { offset: 0, value: "scale(2, 1 )" }
+      ]
+   }
+});
+
+t9.play();
+
+just.tools.player(t9);
+ ////////////////////////////////////
+ var t10 = just.animate({
+   targets: "#boton10",
+   duration: 1000,
+   props: {
+     innerHTML: ['animación', '10'] 
+   },
+   web: {
+      transform: [
+         { offset: 1, value: "scale(1, 1)" },
+         { offset: 2, value: "scale(1, 1)" },
+         { offset: 1, value: "scale(.2, .20)" },
+         { offset: 2, value: "scale(.1, .30)" },
+         { offset: 1, value: "scale(.2, .40)" },
+         { offset: 2, value: "scale(.1, .50)" },
+         { offset: 1, value: "scale(.2, .60)" },
+         { offset: 2, value: "scale(.1, .70)" },
+         { offset: 1, value: "scale(.2, .80)" },
+         { offset: 2, value: "scale(.1, .90)" },
+         { offset: 2, value: "scale(1, 1 )" }
+      ]
+   }
+});
+
+t10.play();
+
+just.tools.player(t10);
